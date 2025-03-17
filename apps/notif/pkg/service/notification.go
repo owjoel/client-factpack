b9c94bb45f1d88fda69064e8e6bd1b4800d8f062
vlingvling <vera.ling.2022@scis.smu.edu.sg>
2025-03-17 17:54:58 +0800

    notif interface and tests

--- apps/notif/pkg/service/notification.go
+++ apps/notif/pkg/service/notification.go
@@ -5,8 +5,19 @@ import (
 	"github.com/owjoel/client-factpack/apps/notif/pkg/api"
 )
 
-// Sends a notification
-func SendNotification(userID, message string) {
+type APINotifier struct{}
+
+// SendNotification sends a message to a user through WebSocket.
+func (a *APINotifier) SendNotification(userID, message string) {
 	fmt.Println("Sending notification to user:", userID)
 	api.SendNotification(userID, message) // Send via WebSocket
+}
+
+type NotificationService struct {
+	Notifier Notifier
+}
+
+// SendNotification calls the Notifier's SendNotification
+func (s *NotificationService) SendNotification(userID, message string) {
+	s.Notifier.SendNotification(userID, message)
 }
